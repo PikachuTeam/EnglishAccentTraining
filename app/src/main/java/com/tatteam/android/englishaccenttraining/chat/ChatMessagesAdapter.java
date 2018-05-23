2ebee48072ae35f0fb28e968a967d5c3260e5433
ThanhNH <huuthanh700@gmail.com>
2018-05-23 22:36:18 +0700

    update ui

--- app/src/main/java/com/tatteam/android/englishaccenttraining/chat/ChatMessagesAdapter.java
+++ app/src/main/java/com/tatteam/android/englishaccenttraining/chat/ChatMessagesAdapter.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.tatteam.android.englishaccenttraining.R;
@@ -19,221 +20,227 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ChatMessagesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-  private static final int VISIBLE_THRESHOLD = 8;
-  private static final int LOAD_MORE = 753;
+    private static final int VISIBLE_THRESHOLD = 8;
+    private static final int LOAD_MORE = 753;
 
-  private Context mContext;
-  private List<ChatMessage> mMessageList = new ArrayList<>();
+    private Context mContext;
+    private List<ChatMessage> mMessageList = new ArrayList<>();
 
-  private RecyclerView mRecyclerView;
+    private RecyclerView mRecyclerView;
 
-  private OnLoadMoreListener mLoadMoreListener;
+    private OnLoadMoreListener mLoadMoreListener;
 
-  private boolean mIsLoading;
-  private boolean mCanLoadMore;
+    private boolean mIsLoading;
+    private boolean mCanLoadMore;
 
-  public void setOnLoadMoreListener(OnLoadMoreListener loadMoreListener) {
-    mLoadMoreListener = loadMoreListener;
-  }
+    public void setOnLoadMoreListener(OnLoadMoreListener loadMoreListener) {
+        mLoadMoreListener = loadMoreListener;
+    }
 
-  public void setCanLoadMore(boolean canLoadMore) {
-    mCanLoadMore = canLoadMore;
-  }
+    public void setCanLoadMore(boolean canLoadMore) {
+        mCanLoadMore = canLoadMore;
+    }
 
-  public ChatMessagesAdapter(Context context, ArrayList<ChatMessage> messageList, RecyclerView recyclerView) {
-    mContext = context;
-    mMessageList = messageList;
-    mRecyclerView = recyclerView;
-    mCanLoadMore = true;
+    public ChatMessagesAdapter(Context context, ArrayList<ChatMessage> messageList, RecyclerView recyclerView) {
+        mContext = context;
+        mMessageList = messageList;
+        mRecyclerView = recyclerView;
+        mCanLoadMore = true;
 
-    mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-      @Override
-      public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-        super.onScrolled(recyclerView, dx, dy);
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
 
-        LinearLayoutManager layoutManager = (LinearLayoutManager) mRecyclerView.getLayoutManager();
+                LinearLayoutManager layoutManager = (LinearLayoutManager) mRecyclerView.getLayoutManager();
 
-        int totalItems = layoutManager.getChildCount();
-        int lastVisibleItem = layoutManager.findLastVisibleItemPosition();
+                int totalItems = layoutManager.getChildCount();
+                int lastVisibleItem = layoutManager.findLastVisibleItemPosition();
 
-        if (mCanLoadMore && lastVisibleItem + VISIBLE_THRESHOLD >= totalItems && !mIsLoading) {
-          if (mLoadMoreListener != null) {
-            mIsLoading = true;
-            showLoadMore();
-            mLoadMoreListener.onLoadMore();
-          }
+                if (mCanLoadMore && lastVisibleItem + VISIBLE_THRESHOLD >= totalItems && !mIsLoading) {
+                    if (mLoadMoreListener != null) {
+                        mIsLoading = true;
+                        showLoadMore();
+                        mLoadMoreListener.onLoadMore();
+                    }
+                }
+            }
+        });
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        switch (viewType) {
+            case ChatMessage.MY_MESSAGE:
+                return new MyMessViewHolder(inflater.inflate(R.layout.item_my_message, parent, false));
+            case ChatMessage.THEIR_MESSAGE:
+                return new TheirMessViewHolder(inflater.inflate(R.layout.item_their_message, parent, false));
+            case LOAD_MORE:
+                return new LoadMoreHolder(inflater.inflate(R.layout.item_load_more, parent, false));
+            default:
+                return new TimeViewHolder(inflater.inflate(R.layout.item_time_message, parent, false));
+
         }
-      }
-    });
-  }
-
-  @NonNull
-  @Override
-  public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    LayoutInflater inflater = LayoutInflater.from(mContext);
-    switch (viewType) {
-      case ChatMessage.MY_MESSAGE:
-        return new MyMessViewHolder(inflater.inflate(R.layout.item_my_message, parent, false));
-      case ChatMessage.THEIR_MESSAGE:
-        return new TheirMessViewHolder(inflater.inflate(R.layout.item_their_message, parent, false));
-      case LOAD_MORE:
-        return new LoadMoreHolder(inflater.inflate(R.layout.item_load_more, parent, false));
-      default:
-        return new TimeViewHolder(inflater.inflate(R.layout.item_time_message, parent, false));
-
     }
-  }
 
-  @Override
-  public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-    if (holder instanceof MyMessViewHolder) {
-      ((MyMessViewHolder) holder).bindData(mMessageList.get(position));
-    } else if (holder instanceof TheirMessViewHolder) {
-      ((TheirMessViewHolder) holder).bindData(mMessageList.get(position));
-    } else if (holder instanceof TimeViewHolder) {
-      ((TimeViewHolder) holder).bindData(mMessageList.get(position));
-    }
-  }
-
-  @Override
-  public int getItemCount() {
-    return mMessageList.size();
-  }
-
-  @Override
-  public int getItemViewType(int position) {
-    if (mMessageList.get(position) == null)
-      return LOAD_MORE;
-    return mMessageList.get(position).viewType;
-  }
-
-  public void showLoadMore() {
-    mRecyclerView.post(new Runnable() {
-      @Override
-      public void run() {
-        mMessageList.add(null);
-        final int loadMoreIndex = mMessageList.size() - 1;
-        notifyItemInserted(loadMoreIndex);
-      }
-    });
-  }
-
-  public void dismissLoadMore() {
-    mRecyclerView.post(new Runnable() {
-      @Override
-      public void run() {
-        mIsLoading = false;
-        int loadMoreIndex = -1;
-        for (int i = mMessageList.size() - 1; i >= 0; i--) {
-          if (mMessageList.get(i) == null) {
-            loadMoreIndex = i;
-            break;
-          }
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof MyMessViewHolder) {
+            ((MyMessViewHolder) holder).bindData(mMessageList.get(position), mContext);
+        } else if (holder instanceof TheirMessViewHolder) {
+            ((TheirMessViewHolder) holder).bindData(mMessageList.get(position));
+        } else if (holder instanceof TimeViewHolder) {
+            ((TimeViewHolder) holder).bindData(mMessageList.get(position));
         }
-        if (loadMoreIndex != -1) {
-          mMessageList.remove(loadMoreIndex);
-          notifyItemRemoved(loadMoreIndex);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMessageList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (mMessageList.get(position) == null)
+            return LOAD_MORE;
+        return mMessageList.get(position).viewType;
+    }
+
+    public void showLoadMore() {
+        mRecyclerView.post(new Runnable() {
+            @Override
+            public void run() {
+                mMessageList.add(null);
+                final int loadMoreIndex = mMessageList.size() - 1;
+                notifyItemInserted(loadMoreIndex);
+            }
+        });
+    }
+
+    public void dismissLoadMore() {
+        mRecyclerView.post(new Runnable() {
+            @Override
+            public void run() {
+                mIsLoading = false;
+                int loadMoreIndex = -1;
+                for (int i = mMessageList.size() - 1; i >= 0; i--) {
+                    if (mMessageList.get(i) == null) {
+                        loadMoreIndex = i;
+                        break;
+                    }
+                }
+                if (loadMoreIndex != -1) {
+                    mMessageList.remove(loadMoreIndex);
+                    notifyItemRemoved(loadMoreIndex);
+                }
+            }
+        });
+    }
+
+    public class TimeViewHolder extends RecyclerView.ViewHolder {
+        TextView mTextDate;
+
+        public TimeViewHolder(View itemView) {
+            super(itemView);
+            mTextDate = itemView.findViewById(R.id.tv_time);
         }
-      }
-    });
-  }
 
-  public class TimeViewHolder extends RecyclerView.ViewHolder {
-    TextView mTextDate;
-
-    public TimeViewHolder(View itemView) {
-      super(itemView);
-      mTextDate = itemView.findViewById(R.id.tv_time);
-    }
-
-    public void bindData(ChatMessage chatMessage) {
-      mTextDate.setText("");
-      if (!TextUtils.isEmpty(chatMessage.time)) {
-        try {
-          mTextDate.setText(DateTimeUtils.getChatDateToShow(chatMessage.time));
-        } catch (ParseException e) {
-          e.printStackTrace();
+        public void bindData(ChatMessage chatMessage) {
+            mTextDate.setText("");
+            if (!TextUtils.isEmpty(chatMessage.time)) {
+                try {
+                    mTextDate.setText(DateTimeUtils.getChatDateToShow(chatMessage.time));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-      }
-    }
-  }
-
-  public class TheirMessViewHolder extends RecyclerView.ViewHolder {
-    TextView mTextContent, mTextTimeSent, mTextSender;
-
-    public TheirMessViewHolder(View itemView) {
-      super(itemView);
-      mTextContent = itemView.findViewById(R.id.tv_content);
-      mTextTimeSent = itemView.findViewById(R.id.tv_time_chat);
-      mTextSender = itemView.findViewById(R.id.tv_user_name);
     }
 
-    public void bindData(ChatMessage chatMessage) {
-      mTextContent.setText(chatMessage.content);
+    public class TheirMessViewHolder extends RecyclerView.ViewHolder {
+        TextView mTextContent, mTextTimeSent, mTextSender;
 
-      mTextTimeSent.setText("");
-      if (!TextUtils.isEmpty(chatMessage.time)) {
-        try {
-          mTextTimeSent.setText(DateTimeUtils.getChatTimeToShow(chatMessage.time));
-        } catch (ParseException e) {
-          e.printStackTrace();
+        public TheirMessViewHolder(View itemView) {
+            super(itemView);
+            mTextContent = itemView.findViewById(R.id.tv_content);
+            mTextTimeSent = itemView.findViewById(R.id.tv_time_chat);
+            mTextSender = itemView.findViewById(R.id.tv_user_name);
         }
-      }
 
-      if (!chatMessage.isAdjacent) {
-        mTextSender.setVisibility(View.VISIBLE);
-        mTextSender.setText(chatMessage.from);
-      } else {
-        mTextSender.setVisibility(View.GONE);
-      }
-    }
-  }
+        public void bindData(ChatMessage chatMessage) {
+            mTextContent.setText(chatMessage.content);
 
-  public class MyMessViewHolder extends RecyclerView.ViewHolder {
-    TextView mTextContent, mTextTimeSent;
-    ImageView mImageSend;
+            mTextTimeSent.setText("");
+            if (!TextUtils.isEmpty(chatMessage.time)) {
+                try {
+                    mTextTimeSent.setText(DateTimeUtils.getChatTimeToShow(chatMessage.time));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
 
-    public MyMessViewHolder(View itemView) {
-      super(itemView);
-      mTextContent = itemView.findViewById(R.id.tv_content_my_mess);
-      mTextTimeSent = itemView.findViewById(R.id.tv_time_chat_my_mess);
-      mImageSend = itemView.findViewById(R.id.img_send);
-    }
-
-    public void bindData(ChatMessage chatMessage) {
-      mTextContent.setText(chatMessage.content);
-
-      mTextTimeSent.setText("");
-      if (!TextUtils.isEmpty(chatMessage.time)) {
-        try {
-          mTextTimeSent.setText(DateTimeUtils.getChatTimeToShow(chatMessage.time));
-        } catch (ParseException e) {
-          e.printStackTrace();
+            if (!chatMessage.isAdjacent) {
+                mTextSender.setVisibility(View.VISIBLE);
+                mTextSender.setText(chatMessage.from);
+            } else {
+                mTextSender.setVisibility(View.GONE);
+            }
         }
-      }
-
-      switch (chatMessage.state) {
-        case ChatMessage.STATE_SENDING:
-          mImageSend.setImageResource(R.drawable.ic_sending);
-          break;
-        case ChatMessage.STATE_ERROR:
-          mImageSend.setImageResource(R.drawable.ic_message_error);
-          break;
-        default:
-          mImageSend.setImageResource(R.drawable.ic_sent);
-          break;
-      }
     }
-  }
 
-  class LoadMoreHolder extends RecyclerView.ViewHolder {
+    public class MyMessViewHolder extends RecyclerView.ViewHolder {
+        TextView mTextContent, mTextTimeSent;
+        ImageView mImageSend;
+        RelativeLayout mLayoutMessage;
 
-    public LoadMoreHolder(View itemView) {
-      super(itemView);
+        public MyMessViewHolder(View itemView) {
+            super(itemView);
+            mTextContent = itemView.findViewById(R.id.tv_content_my_mess);
+            mTextTimeSent = itemView.findViewById(R.id.tv_time_chat_my_mess);
+            mImageSend = itemView.findViewById(R.id.img_send);
+            mLayoutMessage = itemView.findViewById(R.id.layout_message);
+        }
+
+        public void bindData(ChatMessage chatMessage, Context context) {
+            mTextContent.setText(chatMessage.content);
+
+            mTextTimeSent.setText("");
+            if (!TextUtils.isEmpty(chatMessage.time)) {
+                try {
+                    mTextTimeSent.setText(DateTimeUtils.getChatTimeToShow(chatMessage.time));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+            mImageSend.setVisibility(View.GONE);
+
+            switch (chatMessage.state) {
+                case ChatMessage.STATE_SENDING:
+                    mLayoutMessage.setBackgroundColor(context.getResources().getColor(R.color.my_chat_sending));
+//                    mImageSend.setImageResource(R.drawable.ic_sending);
+                    break;
+                case ChatMessage.STATE_ERROR:
+                    mLayoutMessage.setBackgroundColor(context.getResources().getColor(R.color.my_chat_sending));
+                    mImageSend.setVisibility(View.VISIBLE);
+                    break;
+                default:
+                    mLayoutMessage.setBackgroundColor(context.getResources().getColor(R.color.my_chat_bg));
+//                    mImageSend.setImageResource(R.drawable.ic_sent);
+                    break;
+            }
+        }
     }
-  }
 
-  interface OnLoadMoreListener {
-    void onLoadMore();
-  }
+    class LoadMoreHolder extends RecyclerView.ViewHolder {
+
+        public LoadMoreHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    interface OnLoadMoreListener {
+        void onLoadMore();
+    }
 }
